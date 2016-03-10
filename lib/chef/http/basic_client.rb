862f1e6dd732906bd13612980116ff716b631379
Lamont Granquist <lamont@scriptkiddie.org>
2016-03-09 16:51:11 -0800

    start the client

--- lib/chef/http/basic_client.rb
+++ lib/chef/http/basic_client.rb
@@ -138,6 +138,7 @@ class Chef
 
         http_client.read_timeout = config[:rest_timeout]
         http_client.open_timeout = config[:rest_timeout]
+        http_client.start
         http_client
       end
 
