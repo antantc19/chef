d4c488e698e064f5c3ec2d6860a26c03669c42ec
Lamont Granquist <lamont@scriptkiddie.org>
2016-07-01 16:48:27 -0700

    unbuffered i/o for helper
    and return something if its not found as well

--- lib/chef/provider/package/dnf_helper.rb
+++ lib/chef/provider/package/dnf_helper.rb
@@ -4,13 +4,14 @@ def whatprovides(package_name)
   provides = `dnf repoquery -q --latest-limit 1 --whatprovides #{package_name}`
   provides.each_line do |line|
     if line =~ /^(\S+)\-(\S+)\-(\S+)\.(\S+)/
-      puts "#{$1} #{$2}-#{$3}.#{$4}"
-      break
+      STDOUT.syswrite "#{$1} #{$2}-#{$3}.#{$4}\n"
+      return nil
     end
   end
+  STDOUT.syswrite "#{package_name}\n"
 end
 
-while line = STDIN.gets.chomp
+while line = STDIN.sysread(4096).chomp
   args = line.split(/\s+/)
   case args.shift
   when "whatprovides"
